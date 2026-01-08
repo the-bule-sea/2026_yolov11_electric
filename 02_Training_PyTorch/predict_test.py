d9223c6c2c3fc34d4190758de59d52187e3a4384
the-bule-sea <2653935465@qq.com>
2026-01-08 10:25:12 +0800

    feat(test): 更新测试图片路径

--- 02_Training_PyTorch/predict_test.py
+++ 02_Training_PyTorch/predict_test.py
@@ -8,7 +8,7 @@ model = YOLO(model_path)
 
 # 2. 指定一张图片进行测试
 # 你可以找一张没参与训练的网图，或者验证集里的图
-source_image = r'D:\Document\000_school\2025_4up\yolo_electric\02_Training_PyTorch/test_images\test10.jpg' # 换成你具体的图片路径
+source_image = r'D:\Document\000_school\2025_4up\yolo_electric\02_Training_PyTorch\test_images\test11.jpeg' # 换成你具体的图片路径
 
 # 3. 开始推理
 # conf=0.25: 置信度阈值，只有超过 25% 把握的才画框
